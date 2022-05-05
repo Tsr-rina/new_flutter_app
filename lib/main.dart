24618d65edfb7b8728dfae6698b166daf6ea4874
rinas <topi2frie3elec@gmail.com>
2022-05-05 16:38:33 +0900

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
