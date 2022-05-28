fcca41666eabb2303ec090721687518f9430196e
rinas <topi2frie3elec@gmail.com>
2022-05-28 17:01:43 +0900

    add firestore code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 // import 'package:english_words/english_words.dart';
@@ -442,3 +443,67 @@ class AddPostPage extends StatelessWidget {
 //   _RandomWordsState createState() => _RandomWordsState();
 
 // }
+
+class MyFirestorePage extends StatefulWidget {
+  const MyFirestorePage({Key? key}): super(key: key);
+  @override
+  _MyFirestorePageState createState() => _MyFirestorePageState();
+}
+
+class _MyFirestorePageState extends State<MyFirestorePage> {
+  // 作成したドキュメント一覧
+  List<DocumentSnapshot> documentList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text("コレクション+ドキュメント作成"),
+              onPressed: () async {
+                // ドキュメント作成
+                await FirebaseFirestore.instance
+                .collection('users') //コレクションID
+                .doc('id_abc') //ドキュメントID
+                .set({'name':'鈴木','age':40});
+              },
+            ),
+            ElevatedButton(
+              child: const Text("サブコレクション+ドキュメント作成"),
+              onPressed: () async {
+                // サブコレクション内にドキュメント作成
+                await FirebaseFirestore.instance
+                .collection('users') //コレクションID
+                .doc('id_abc') // ドキュメントID >>usersコレクション内のドキュメント
+                .collection('orders') // サブコレクションID
+                .doc('id_123') // ドキュメントID >> サブコレクション内のドキュメント
+                .set({'price':600, 'date':'9/13'}); //データ
+              },
+            ),
+            ElevatedButton(
+              child: const Text("ドキュメント一覧取得"),
+              onPressed: () async {
+                // コレクション内のドキュメント一覧を取得
+                final snapshot = await FirebaseFirestore.instance.collection('users').get();
+                // 取得したドキュメント一覧をUIに反映
+                setState(() {
+                  documentList = snapshot.docs;
+                });
+              },
+            ),
+            // コレクション内のドキュメント一覧を表示
+            Column(
+              children: documentList.map((document){
+                return ListTile(
+                  title: Text('${document['name']}さん'),
+                  subtitle: Text('${document['age']}歳'),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
