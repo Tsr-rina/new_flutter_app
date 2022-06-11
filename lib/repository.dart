a0ab7d714dbd3e1f9e6fe84af16f6c2c1a0fb2eb
rinas <topi2frie3elec@gmail.com>
2022-06-11 16:38:44 +0900

    change page

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'profile.dart';
 import 'browsing.dart';
@@ -18,12 +20,74 @@ class _RepositoryPageState extends State<RepositoryPage>{
     return Scaffold(
       appBar: AppBar(
         title: const Text("Repository"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              // ログアウト処理
+              // 内部で保持しているログイン情報等が初期化される
+              await FirebaseAuth.instance.signOut();
+              // ログイン画面に遷移してRepository画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context){
+                  return const LoginPage();
+                }),
+              );
+            },
+          ),
+        ],
       ),
       body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text("ここに作品を並べる"),
+        children:[
+          Expanded(
+            // StreamBuilder
+            // 非同期処理の結果をもとにWidget
+            child: StreamBuilder<QuerySnapshot>(
+              // 投稿メッセージ一覧を取得(非同期処理)
+              // 投稿日時でソート
+              stream: FirebaseFirestore.instance
+              .collection('posts')
+              .orderBy('date')
+              .snapshots(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  // 取得した投稿メッセージ一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      // if(document['email']==widget.user.email){
+                      //   final j_email = widget.user.email;
+                      //   final j_text = widget.user.email;
+                      // }
+                      return Card(
+                        child: ListTile(
+                          title: Text(document['text']),
+                          subtitle: Text(document['email']),
+                          // 自分の投稿メッセージの場合は削除ボタンを表示
+                          trailing: document['email'] == widget.user.email?
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () async {
+                              // 投稿メッセージのドキュメントを削除
+                              await FirebaseFirestore.instance
+                              .collection('posts')
+                              .doc(document.id)
+                              .delete();
+                            },
+                          )
+                          :null,
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+                // データが読み込み中の場合
+                return const Center(
+                  child: Text("読み込み中..."),
+                );
+              },
+            ),
           ),
         ],
       ),
